2bbfb7d894cc172b4d2c70aca0065fa931ba2d69
yocosaka <sakayoco55@gmail.com>
2021-04-15 22:27:45 +0900

    Add order_by_updated and order_by_title to item model and use it in items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: %i[show update destroy]
 
   def index
-    @items = Item.all
+    @items = Item.all.order_by_title
     render json: @items, status: 200
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   has_many :records, dependent: :destroy
   has_many :users, through: :records
   validates_presence_of :title, :unit
+
+  scope :order_by_updated, -> { order(updated_at: :desc) }
+  scope :order_by_title, -> { order(title: :asc) }
 end
