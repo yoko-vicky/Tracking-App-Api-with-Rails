7e9bbbd21363394f50f929a3b11fd68ae70fd078
yocosaka <sakayoco55@gmail.com>
2021-04-16 15:54:19 +0900

    Fix params setting in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 
   # LOGIN
   def create
-    @user = User.find_by(username: session_params[:username])
+    @user = User.find_by(username: user_params[:username])
 
-    if @user&.authenticate(session_params[:password])
+    if @user&.authenticate(user_params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { logged_in: true, user: @user, token: token }
     elsif @user
@@ -29,10 +29,10 @@ class SessionsController < ApplicationController
   private
 
   def set_user
-    @user = User.find_by(username: session_params[:username])
+    @user = User.find_by(username: user_params[:username])
   end
 
-  def session_params
-    params.require(:session).permit(:username, :password)
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
